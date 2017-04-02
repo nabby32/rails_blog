490a759db9f69681745624beb335c3140122cbbb
Liam Brenner <franzymope@gmail.com>
2017-04-02 12:44:40 -0400

    Added delete functionality to comments.

--- blogger/app/controllers/comments_controller.rb
+++ blogger/app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+	include CommentsHelper
+
 	def create
 		@comment = Comment.new(comment_params)
 		@comment.article_id = params[:article_id]
@@ -6,8 +8,16 @@ class CommentsController < ApplicationController
 		redirect_to article_path(@comment.article)
 	end
 
-	def comment_params
-		params.require(:comment).permit(:author_name, :body)
+	#def comment_params
+	#	params.require(:comment).permit(:author_name, :body)
+	#end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		flash.notice = "Comment by #{@comment.author_name} was deleted."
+		redirect_to article_path(@article)
 	end
 
 end
